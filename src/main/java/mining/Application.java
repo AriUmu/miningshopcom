89c15a2251cd3a5ac7ef54ab98dd07df3bfc29e4
Arina Umushkina <Arina_Umushkina@epam.com>
2018-03-10 12:15:42 +0300

    added shema.sql

--- src/main/java/mining/Application.java
+++ src/main/java/mining/Application.java
@@ -4,8 +4,12 @@ package mining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class Application {
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/mining/config/DataSourceConfig.java
@@ -0,0 +1,77 @@
+package mining.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+
+//    @Value("${dbSQLSchema}")
+//    private Resource dbSQLSchema;
+//
+//    @Value("${dbSQLTestData}")
+//    private Resource dbSQLTestData;
+
+    @Value("${hibernateDialect}")
+    private String hibernateDialect;
+
+    @Value("${showSQL}")
+    private String showSQL;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public Properties hibernateProperties() {
+        Properties hibernateProp = new Properties();
+
+        hibernateProp.put("hibernate.dialect", hibernateDialect);
+        hibernateProp.put("hibernate.hbm2ddl.auto", "update");
+        hibernateProp.put("hibernate.show_sql", showSQL);
+        hibernateProp.put("hibernate.format_sql", true);
+
+        return hibernateProp;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(driverClassName);
+        ds.setUrl(url);
+        ds.setUsername(dbUser);
+        ds.setPassword(dbPassword);
+
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+//        populator.addScript(dbSQLSchema);
+//        populator.addScript(dbSQLTestData);
+        DatabasePopulatorUtils.execute(populator, ds);
+
+        return ds;
+    }
+}
